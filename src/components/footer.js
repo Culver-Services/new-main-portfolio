5aad40173df7cfff62eef93dd3d5e53e04d64f84
Chadwick Culver <83472455+Culver-Services@users.noreply.github.com>
2021-12-02 15:50:02 -0500

    Add files via upload

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Chadwick Culver <a href="https://culver.services.com/">Portfolio</a> and{' '}
+      Chadwick Culver <a href="https://culver.services/">Portfolio</a> and{' '}
       <a href="https://github.com/Culver-Services?tab=repositories">Github</a> &middot;{' '}
       <a href="https://github.com/Culver-Services/new-main-portfolio">Source</a>
     </div>
